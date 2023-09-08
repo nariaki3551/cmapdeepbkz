1151134562ce45353fe5ab8c2a9285f923e451aa
Nariaki Tateiwa <nariaki3551@gmail.com>
2023-09-08 15:23:59 +0900

    update deepbkz and enum core function (#12)
    * replace DeepBKZ.h, Enum.h settings/default.set
    * add a setting file for test

--- src/DeepBKZ/DeepBKZ/Enum.h
+++ src/DeepBKZ/DeepBKZ/Enum.h
@@ -22,7 +22,7 @@ Reference:
 - "Lattice Enumeration using Extreme Pruning" by Gama-Nguyen-Regev (EUROCRYPT 2011)
 - Appendix B, Algorithm 2(Pruned Enumeration)
 ***********************************************************************************/
-inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h) 
+inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h)
 {
 	/* Assume that GSO information is already input */
 	int i, j, k, last_nonzero, n = NumRows, flag = 0;
@@ -33,9 +33,9 @@ inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h)
 	Vec<double> c; c.SetLength(n);
 	Vec<int> w; w.SetLength(n);
 	double dtmp;
-	
+
 	/* Step 1 */
-	for (i = 1; i <= n+1; ++i) { 
+	for (i = 1; i <= n+1; ++i) {
 		for (j = 1; j <= n; ++j) { sigma(i, j) = 0.0; }
 	}
 	for (i = 0; i <= n; ++i) { r[i] = i; }
@@ -46,7 +46,7 @@ inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h)
 	/* Step 3: current combination of integers for a lattice vector */
 	for (i = 1; i <= n; ++i) { v(i) = 0; }
 	v(g) = 1;
-	
+
 	/* Step 4: centers */
 	for (i = 1; i <= n; ++i) { c(i) = 0.0; }
 
@@ -54,18 +54,40 @@ inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h)
 	for (i = 1; i <= n; ++i) { w(i) = 0; }
 
 	/* Step 6: largest index i for which v_i != 0 */
-	last_nonzero = 1;
+	last_nonzero = g;
+
+#if 1
+	int n_sub = h-g+1, p, t;
+	long double templ, temp;
+	Vec<double> eta; eta.SetLength(n_sub);
+	if (n_sub >= 35) {
+		p = 15;
+		templ = sqrtl(B(g));
+		for (t = 2; t<= n_sub; ++t) {
+			temp = tgammal(0.50*(t+1.0));
+			eta[t-1] =  powl(temp*pow(2.0, -1.0*p)*templ, 2.0/(t - 1.0))/M_PI;
+			templ *= sqrtl(B(g+t-1));
+		}
+		for (i = 1; i<n_sub; ++i) {
+			SR[i] -= eta[i];
+			if (SR[i] <= DBL_EPSILON) {
+				return false;
+			}
+		}
+	}
+#endif
 
 	k = g;
 	while (true) {
-		/* Step 9: compute norm squared of current node */
+		/* Step 9: compute squared norm of current node */
 		dtmp = v(k) - c(k);
 		dtmp *= dtmp;
 		rho(k) = rho(k+1) + dtmp * B(k);
-		if (rho(k) <= SR(n+1-k)) {
+		// if (rho(k) <= SR(n+1-k)) {
+		if (rho(k) <= SR[k-g]) {
 			if (k == g) {
 				/* A solution is found */
-				++flag; 
+				++flag;
 				for (i=1; i<=n; ++i) { result(i) = v(i); }
 				for (i=1; i<=n; ++i) { SR(i) = min(0.99*rho(g), SR(i)); }
 				// SR = 0.99*rho(g); /* SR update */
@@ -84,7 +106,8 @@ inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h)
 			++k;
 			if (k == h+1) {
 				if (flag > 0) {
-					return true; 
+					// cout << result << endl;
+					return true;
 				} else {
 					/* There is no solution */
 					for (i = 1; i <= n; ++i) { result(i) = 0; }
@@ -94,11 +117,11 @@ inline bool lattice::Enum(Vec<int> &result, Vec<double> SR, int g, int h)
 			r[k - 1] = k;
 			if (k >= last_nonzero) {
 				last_nonzero = k;
-				++v(k); 
+				++v(k);
 			} else {
 				if (v(k) > c(k)) { v(k) -= w(k); }
 				else { v(k) += w(k); }
-				++w(k); 
+				++w(k);
 			}
 		}
 	}
